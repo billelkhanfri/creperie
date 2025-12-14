64cb16c7d798350d2503255411d67447007e4b56
billelkhanfr <khanfri.billel@yahoo.fr>
2025-12-14 20:55:45 +0100

    fix deplyement

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
-import ClientLayout from "./components/ClientLayout";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 export const metadata = {
   title: "CAAA — coeur de ville - Toulon",
@@ -10,7 +11,17 @@ export default function RootLayout({ children }) {
   return (
     <html data-theme="corporate" lang="fr">
       <body className="flex flex-col min-h-screen">
-        <ClientLayout>{children}</ClientLayout>
+        {/* Header */}
+        <header className="border-b border-gray-200 max-w-7xl w-full mx-auto">
+          <Navbar />
+        </header>
+
+        {/* Contenu */}
+
+        <main className="flex-1  mx-auto w-full ">{children}</main>
+
+        {/* Footer */}
+        <Footer></Footer>
       </body>
     </html>
   );
